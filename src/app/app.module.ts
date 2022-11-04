dc131dfc080877c7245b8010b84a91696f9ec04a
LucaSantosSP <54375485+LucaSantosSP@users.noreply.github.com>
2022-11-04 19:40:09 -0300

    Inserido o RouterModule no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -6,10 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { TasksService } from './services/tasks.service';
+import { TimerService } from './services/timer.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'timer', component: TimerComponent },
+      { path: 'tasks', component: TasksComponent },
+    ]),
+  ],
   declarations: [AppComponent, HomeComponent, TasksComponent, TimerComponent],
   bootstrap: [AppComponent],
+  providers: [TasksService, TimerService],
 })
 export class AppModule {}
