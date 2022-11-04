7df800e18ae160a9a73e2882b181a6e5ff9a414c
LucaSantosSP <54375485+LucaSantosSP@users.noreply.github.com>
2022-11-04 19:34:06 -0300

    Adicionado components & services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, TasksComponent, TimerComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
